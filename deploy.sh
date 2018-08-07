7b24561245ffc5732bd77780dc2809ae83056b8c
ZBCrazyFire <1312296904@qq.com>
2018-08-07 23:22:41 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,14 @@ killPort()
 {
 	port=8090
 	#根据端口号查询对应的pid
-	pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
-
+	pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }')
+	echo "tomcat Id list :$pid"
 	#杀掉对应的进程，如果pid不存在，则不执行
-	if [  -n  "$pid"  ];  then
-		kill  -9  $pid;
+	if [ "pid" = ""]
+	then
+		echo "no tomcat pid alive"
+	else 
+		kill -9 $pid
 	fi
 }
 
