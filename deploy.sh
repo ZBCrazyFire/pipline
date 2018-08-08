1a8c575cfbab18d9d12602a5de791961f5b9a169
ZBCrazyFire <1312296904@qq.com>
2018-08-09 00:04:18 +0800

    修改脚本，根据端口kill进程

--- deploy.sh
+++ deploy.sh
@@ -18,11 +18,23 @@ killTomcat()
 	fi
 }
 
+killPort()
+{
+	port=8090
+	#根据端口号查询对应的pid
+	pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+
+	#杀掉对应的进程，如果pid不存在，则不执行
+	if [  -n  "$pid"  ];  then
+		kill  -9  $pid;
+	fi
+}
+
 cd $PROJ_PATH/pipline
 mvn clean install
 
 #停止tomcat
-killTomcat
+killPort
 
 #删除原有工程
 #rm -rf $TOMCAT_APP_PATH/webapps/ROOT
