216694eb5830fa08f9c2f9a3dfd52eea1e0a930c
ZBCrazyFire <1312296904@qq.com>
2018-08-08 23:51:08 +0800

    部署脚本

--- deploy.sh
+++ deploy.sh
@@ -32,23 +32,6 @@ mvn clean install
 
 #停止tomcat
 killTomcat()
-
-#删除原有工程
-#rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-#rm -rf $TOMCAT_APP_PATH/webapps/ROOT.war
-#rm -rf $TOMCAT_APP_PATH/webapps/roder.war
-#rm -rf $WORK_PATH/PIPLINE.jar
-
-
-#复制新的工程
-#cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-#cp $PROJ_PATH/pipline/target/pipline-0.0.1-SNAPSHOT.jar $WORK_PATH/
-
-#cd $WORK_PATH/
-#mv order.war ROOT.war
-#mv pipline-0.0.1-SNAPSHOT.jar PIPLINE.jar
-
-
 moveFile()
 #启动tomcat
 cd $WORK_PATH/
